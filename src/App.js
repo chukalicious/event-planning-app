13cb7400f33f25fb72cda4cdca34371f48438b5b
Katiuska Alicea <katiuskaalicea@gmail.com>
2023-04-18 18:32:06 -0700

    Created Date Picker field dumb component. Installed date libraries to use with Material UI date picker.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import { appTheme } from "./themes/appTheme";
 
+import ResponsiveDatePickers from "./components/DatePickerComponent/DatePicker";
+
 function App() {
   return (
     <>
       <CssBaseline />
 
       <ThemeProvider theme={appTheme}>
-        <div className="App"></div>
+        <div className="App">
+          this is the App
+          <ResponsiveDatePickers />{" "}
+        </div>
       </ThemeProvider>
     </>
   );
